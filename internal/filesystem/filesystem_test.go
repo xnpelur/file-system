79fc75cc278574588fd35115840ba6379cb7e111
xnpelur <michael.s5102@gmail.com>
2023-11-15 17:43:58 +0300

    New filesystem tests

--- internal/filesystem/filesystem_test.go
+++ internal/filesystem/filesystem_test.go
@@ -1,13 +1,18 @@
 package filesystem
 
 import (
+	"errors"
+	"file-system/internal/errs"
+	"fmt"
+	"io"
+	"strings"
 	"testing"
 )
 
 func TestFilesystemIntegration(t *testing.T) {
 	fs, err := FormatFilesystem(1024*1024, 1024)
 	if err != nil {
-		t.Fatalf("Failed to open filesystem: %v", err)
+		t.Fatalf("Failed to create filesystem: %v", err)
 	}
 	defer fs.CloseDataFile()
 
@@ -60,8 +65,121 @@ func TestFilesystemIntegration(t *testing.T) {
 		}
 
 		content, err := fs.ReadFile("test.txt")
-		if err == nil {
-			t.Errorf("DeleteFile error: fs.ReadFile does not return error after file deletion. File content: %s", content)
+		if !errors.Is(err, errs.ErrRecordNotFound) {
+			t.Errorf("DeleteFile error: record was not properly deleted. File content: %s", content)
 		}
 	})
+}
+
+func TestDeleteDirectoryWithNestedFiles(t *testing.T) {
+	fs, err := FormatFilesystem(1024*1024, 1024)
+	if err != nil {
+		t.Fatalf("Failed to create filesystem: %v", err)
+	}
+	defer fs.CloseDataFile()
+
+	nestingLevel := 10
+
+	for i := 1; i <= nestingLevel; i++ {
+		fileName := fmt.Sprintf("file%d", i)
+		dirName := fmt.Sprintf("dir%d", i)
+
+		if err = fs.CreateDirectory(dirName); err != nil {
+			t.Fatalf("Failed to create directory %s: %v", dirName, err)
+		}
+		if err = fs.ChangeDirectory(dirName); err != nil {
+			t.Fatalf("Failed to change directory to %s: %v", dirName, err)
+		}
+		if err = fs.CreateFile(fileName, ""); err != nil {
+			t.Fatalf("Failed to create file %s: %v", fileName, err)
+		}
+	}
+
+	dotDotComponents := make([]string, nestingLevel)
+	for i := range dotDotComponents {
+		dotDotComponents[i] = ".."
+	}
+	pathToRoot := strings.Join(dotDotComponents, "/")
+
+	if err = fs.ChangeDirectory(pathToRoot); err != nil {
+		t.Fatalf("Failed to go back to root - cd %s: %v", pathToRoot, err)
+	}
+
+	if err = fs.DeleteFile("dir1", fs.currentDirectory, fs.currentDirectoryInode); err != nil {
+		t.Fatalf("Failed to delete dir1: %v", err)
+	}
+}
+
+func TestDataFileIdempotency(t *testing.T) {
+	fs, err := FormatFilesystem(1024*1024, 1024)
+	if err != nil {
+		t.Fatalf("Failed to create filesystem: %v", err)
+	}
+	defer fs.CloseDataFile()
+
+	savedContent, err := io.ReadAll(fs.dataFile)
+	if err != nil {
+		t.Fatalf("Failed to read data file: %v", err)
+	}
+
+	if err = fs.CreateDirectory("dir"); err != nil {
+		t.Fatalf("Failed to create directory: %v", err)
+	}
+	if err = fs.CreateFile("file", "file content"); err != nil {
+		t.Fatalf("Failed to create file: %v", err)
+	}
+	if err = fs.ChangeDirectory("dir"); err != nil {
+		t.Fatalf("Failed to change current directory: %v", err)
+	}
+	if err = fs.CreateFile("otherfile", "other file content"); err != nil {
+		t.Fatalf("Failed to create file: %v", err)
+	}
+	if err = fs.CreateDirectory("otherdir"); err != nil {
+		t.Fatalf("Failed to create directory: %v", err)
+	}
+	if err = fs.ChangeDirectory(".."); err != nil {
+		t.Fatalf("Failed to change current directory: %v", err)
+	}
+
+	if err = fs.DeleteFile("dir", fs.currentDirectory, fs.currentDirectoryInode); err != nil {
+		t.Fatalf("Failed to delete directory: %v", err)
+	}
+	if err = fs.DeleteFile("file", fs.currentDirectory, fs.currentDirectoryInode); err != nil {
+		t.Fatalf("Failed to delete file: %v", err)
+	}
+
+	currentContent, err := io.ReadAll(fs.dataFile)
+	if err != nil {
+		t.Fatalf("Failed to read data file: %v", err)
+	}
+
+	if string(currentContent) != string(savedContent) {
+		t.Errorf("File content mismatch. Expected: %s, Got: %s", savedContent, currentContent)
+	}
+	diffIndex := findFirstDifference(savedContent, currentContent)
+
+	if diffIndex != -1 {
+		expectedByte := savedContent[diffIndex]
+		gotByte := currentContent[diffIndex]
+		t.Errorf("File content mismatch at byte index %d. Expected: %x, Got: %x", diffIndex, expectedByte, gotByte)
+	}
+}
+
+func findFirstDifference(slice1, slice2 []byte) int {
+	minLen := len(slice1)
+	if len(slice2) < minLen {
+		minLen = len(slice2)
+	}
+
+	for i := 0; i < minLen; i++ {
+		if slice1[i] != slice2[i] {
+			return i
+		}
+	}
+
+	if len(slice1) == len(slice2) {
+		return -1
+	}
+
+	return minLen
 }
